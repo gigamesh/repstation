595c169cd8922d3e3f4b6eb565b5787a5c9d4ba1
Matt Masurka <m.masurka@gmail.com>
2023-06-26 10:00:16 -0700

    installs solady

--- src/Repstation.sol
+++ src/Repstation.sol
@@ -1,4 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import {Ownable} from "solady/auth/Ownable.sol";
 
-contract Repstation {}
+/**
+ * @title Repstation
+ * @author @gigamesh
+ * @notice An onchain reputation system built on the Ethereum Attestation Service
+ */
+contract Repstation is Ownable {
+    struct Account {
+        uint256 rep;
+        uint32 createdAt;
+    }
+
+    mapping(address => Account) public accounts;
+
+    function initialize() public {
+        _initializeOwner(msg.sender);
+    }
+
+    function attest() public {
+        // TODO: calculate rep change & pass along the attestatoin to EAS
+    }
+}
